b3c0ff7a156425f0ff96a2ef1494fde182ecc757
huwsua <98215473+huwsua@users.noreply.github.com>
2024-06-17 14:39:37 +0600

    Update myAwesomeProgram.java

--- myAwesomeProgram.java
+++ myAwesomeProgram.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.awt.*;
 
 public class myAwesomeProgram implements ActionListener{
+   
    JButton yesButton; 
    JButton noButton;     
    JLabel text = new JLabel();
@@ -15,6 +16,7 @@ public class myAwesomeProgram implements ActionListener{
    Random ranLoc = new Random();
   
   public static void main(String[] args) {
+    
     myAwesomeProgram gui = new myAwesomeProgram();
     gui.go();
   }
@@ -38,7 +40,8 @@ public class myAwesomeProgram implements ActionListener{
     noButton.addMouseListener(new MouseAdapter() {    //Randomizes the position of the button everytime the mouse enters the area of the button
        @Override
        public void mouseEntered(MouseEvent e) {
-          noButton.setBounds(ranLoc.nextInt(700), ranLoc.nextInt(500),100,50);
+         
+         noButton.setBounds(ranLoc.nextInt(700), ranLoc.nextInt(500),100,50);
        }
     });
     
@@ -49,6 +52,7 @@ public class myAwesomeProgram implements ActionListener{
     frame.getContentPane().add(text);
     frame.setLayout(null);
     frame.setSize(800,600);
+    frame.setVisible(true);
     
     frame.setLocation( ranLoc.nextInt(800), ranLoc.nextInt(600) ); // Randomizes the frame location when closed
     
@@ -92,11 +96,10 @@ public class myAwesomeProgram implements ActionListener{
       sureReply = false;
     }
     
-      
-    frame.setVisible(true);
   }
   
   public void actionPerformed(ActionEvent event) {
+    
     if ( event.getSource() == yesButton && textCount!=1 ) {
       for (int i = 0; i < frameCount; i++) {   // Disposes all of the frames before closing when yes is clicked
         if (frames[i] != null) {                // in the "Are you the best teacher" prompt
@@ -104,6 +107,7 @@ public class myAwesomeProgram implements ActionListener{
         }
       }
       ending();                                      // Calls the ending screen after disposing 
+       
     } else if ( event.getSource() == yesButton ) {  
       sureReply = true;                              // If textCount is 1 which is the "Are you sure" prompt 
       go();                                          // and the user presses No (they are not sure) 
@@ -116,6 +120,7 @@ public class myAwesomeProgram implements ActionListener{
   }
 
   public void ending() {
+      
       JFrame eframe = new JFrame();
       eframe.setResizable(false);
       JTextArea textarea = new JTextArea(
@@ -141,7 +146,7 @@ public class myAwesomeProgram implements ActionListener{
                    "                               * * *   * *       * * * *               *                 * *     \n" +     
                    "                                  * * * * * * * *                     * *       * * * * *        \n" +    
                    "                                                                         * * * * *               \n" +
-                   "                             Correct !!!"                                                        //Portrait de la masterpiece
+                   "                             Correct !!!"                                                        // Master potrait
                                         );
       if ( frameCount > 2 ) {
          textarea.append(" You can exit now ");
@@ -150,7 +155,7 @@ public class myAwesomeProgram implements ActionListener{
       textarea.setEditable(false);
       textarea.setBounds(10,10,800,500);
       textarea.setFont(new Font("Monospaced", Font.PLAIN,14)); 
-      eframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      eframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     // Allows to exit finally
       
       eframe.getContentPane().setLayout(null);
       eframe.getContentPane().setBackground(Color.white);
